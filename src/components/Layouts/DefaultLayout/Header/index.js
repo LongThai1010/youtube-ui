01f480787ae7599c0f2c2a4e1ce5a60e312aaef0
long thai <volelongthaipnt@gmail.com>
2023-06-19 22:18:48 +0700

    xây dựng header #1

--- src/components/Layouts/DefaultLayout/Header/index.js
+++ src/components/Layouts/DefaultLayout/Header/index.js
@@ -1,12 +1,31 @@
+import YoutubeSearchedForIcon from '@mui/icons-material/YoutubeSearchedFor';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
+console.log(images);
+
 function Header() {
-    return <div className={cx('wrapper')}>
-        <div className={cx('inner')}></div>
-    </div>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="youtube" />
+                </div>
+                <div className={cx('search')}>
+                    <input placeholder="Search" />
+
+                    <button className={cx('search-btn')}>
+                        <YoutubeSearchedForIcon />
+                    </button>
+                </div>
+
+                <div className={cx('action')}></div>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
